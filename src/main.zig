4918407e331194a3c027b9a286ac325018d863e6
seedse <loganbreezestl@gmail.com>
2026-03-26 19:27:28 -0500

    further improvements

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,7 @@ const usage =
     \\
 ;
 
+const heap_size = 3 * 1024 * 1024;
 const salt_size = 32;
 const key_size = Cipher.key_length;
 const buf_size = 2 * 1024 * 1024;
@@ -19,21 +20,29 @@ const nonce_size = Cipher.nonce_length;
 const overhead = Cipher.tag_length;
 const capacity = nonce_size + overhead + buf_size;
 
-fn setEcho(enable: bool) !void {
+fn setEcho(io: std.Io, enable: bool) !void {
     switch (builtin.os.tag) {
         .windows => {
             const ENABLE_ECHO_MODE: u32 = 0x0004;
 
-            const mode = std.os.windows.CONSOLE.USER_IO.GET_MODE.Data;
+            var mode = std.os.windows.CONSOLE.USER_IO.GET_MODE;
 
-            const newMode = if (enable) mode | ENABLE_ECHO_MODE else mode & ~ENABLE_ECHO_MODE;
+            if (try mode.operate(io, std.Io.File.stdin()) != .SUCCESS) {
+                return error.OperateFailed;
+            }
 
-            _ = std.os.windows.CONSOLE.USER_IO.SET_MODE(newMode);
+            const newMode = if (enable) mode.Data | ENABLE_ECHO_MODE else mode.Data & ~ENABLE_ECHO_MODE;
+
+            mode = std.os.windows.CONSOLE.USER_IO.SET_MODE(newMode);
+
+            if (try mode.operate(io, std.Io.File.stdin()) != .SUCCESS) {
+                return error.OperateFailed;
+            }
         },
         else => {
             const fd = std.Io.File.stdin().handle;
 
-            var termios: std.posix.termios = try std.posix.tcgetattr(fd);
+            var termios = try std.posix.tcgetattr(fd);
 
             termios.lflag.ECHO = enable;
 
@@ -43,8 +52,8 @@ fn setEcho(enable: bool) !void {
 }
 
 fn getPassword(io: std.Io, allocator: std.mem.Allocator, reprompt: bool) ![]u8 {
-    try setEcho(false);
-    defer setEcho(true) catch @panic("Failed to re-enable terminal echo");
+    try setEcho(io, false);
+    defer setEcho(io, true) catch @panic("Failed to re-enable terminal echo");
 
     var stdin_buf: [1024]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(io, &stdin_buf);
@@ -55,8 +64,8 @@ fn getPassword(io: std.Io, allocator: std.mem.Allocator, reprompt: bool) ![]u8 {
 
     try std.Io.File.stdout().writeStreamingAll(io, "\n");
 
-    const p1out = try allocator.alloc(u8, p1.len);
-    @memcpy(p1out, p1);
+    const p1out = try allocator.dupe(u8, p1);
+    errdefer allocator.free(p1out);
 
     if (reprompt) {
         try std.Io.File.stdout().writeStreamingAll(io, "One more time: ");
@@ -79,25 +88,22 @@ const Cli = struct {
     output: ?[]const u8 = null,
 
     const Self = @This();
+    const Error = error{ MissingInput, MissingOutput };
 
-    fn parse(args: []const [:0]const u8) Self {
+    fn parse(args: []const [:0]const u8) Error!Self {
         var out: Self = .{};
 
         var nextIsInput = false;
         var nextIsOutput = false;
         for (args) |arg| {
-            var positionalTriggered = false;
             if (nextIsInput) {
                 out.input = arg;
                 nextIsInput = false;
-                positionalTriggered = true;
+                continue;
             }
             if (nextIsOutput) {
                 out.output = arg;
                 nextIsOutput = false;
-                positionalTriggered = true;
-            }
-            if (positionalTriggered) {
                 continue;
             }
             if (arg.len > 1 and arg[0] == '-') {
@@ -113,9 +119,15 @@ const Cli = struct {
                 }
             }
         }
+        if (nextIsInput) {
+            return Error.MissingInput;
+        }
+        if (nextIsOutput) {
+            return Error.MissingOutput;
+        }
 
-        if (out.input == null) {
-            out.mode = .Help;
+        if (out.input == null and out.mode != .Help) {
+            return Error.MissingInput;
         }
 
         return out;
@@ -137,8 +149,7 @@ fn encrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
     const total = try inputFile.length(io);
 
     var outputFile = if (output) |path| try std.Io.Dir.cwd().createFile(io, path, .{}) else blk: {
-        const fileName = std.fs.path.basename(input);
-        const name = try std.fmt.allocPrint(allocator, "{s}.enc", .{fileName});
+        const name = try std.fmt.allocPrint(allocator, "{s}.enc", .{input});
         defer allocator.free(name);
         break :blk try std.Io.Dir.cwd().createFile(io, name, .{});
     };
@@ -152,8 +163,9 @@ fn encrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
     var counter: u64 = 0;
     var total_read: u64 = 0;
 
-    const progress = std.Progress.start(io, .{ .root_name = "Encrypting file...", .estimated_total_items = @as(usize, total) });
-    defer progress.end();
+    const progress = std.Progress.start(io, .{ .root_name = "processing", .estimated_total_items = 1 });
+    const cipher = progress.startFmt(@as(usize, total), "encrypting {s}", .{input});
+    defer cipher.end();
 
     while (total_read < total) {
         const read = try inputFile.readStreaming(io, &[_][]u8{buf[nonce_size + overhead ..]});
@@ -175,7 +187,7 @@ fn encrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
 
         counter += 1;
 
-        progress.setCompletedItems(total_read);
+        cipher.setCompletedItems(total_read);
     }
 }
 
@@ -184,11 +196,27 @@ fn decrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
     defer allocator.free(password);
 
     var outputFile = if (output) |path| try std.Io.Dir.cwd().createFile(io, path, .{}) else blk: {
-        const baseName = std.fs.path.basename(input);
-        var iter = std.mem.splitAny(u8, baseName, ".enc");
-        const name = iter.next() orelse return error.InvalidFilename;
+        var iter = std.mem.splitSequence(u8, input, ".enc");
 
-        break :blk try std.Io.Dir.cwd().createFile(io, name, .{});
+        var name: ?[]u8 = null;
+        defer if (name) |n| allocator.free(n);
+
+        var n = iter.next();
+        while (n) |next| {
+            n = iter.next();
+
+            if (n == null) {
+                break;
+            }
+
+            const oldN = name;
+            name = try std.fmt.allocPrint(allocator, "{s}{s}", .{ name orelse "", next });
+            if (oldN) |oN| {
+                allocator.free(oN);
+            }
+        }
+
+        break :blk try std.Io.Dir.cwd().createFile(io, name orelse return error.InvalidFilename, .{});
     };
     defer outputFile.close(io);
 
@@ -210,8 +238,9 @@ fn decrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
     var counter: u64 = 0;
     var total_read: u64 = 32;
 
-    const progress = std.Progress.start(io, .{ .root_name = "Decrypting file...", .estimated_total_items = @as(usize, total) });
-    defer progress.end();
+    const progress = std.Progress.start(io, .{ .root_name = "processing", .estimated_total_items = 1 });
+    const cipher = progress.startFmt(@as(usize, total), "decrypting {s}", .{input});
+    defer cipher.end();
 
     while (total_read < total) {
         const read = try inputFile.readStreaming(io, &[_][]u8{buf});
@@ -240,30 +269,32 @@ fn decrypt(io: std.Io, allocator: std.mem.Allocator, input: []const u8, output: 
 
         counter += 1;
 
-        progress.setCompletedItems(total_read);
+        cipher.setCompletedItems(total_read);
     }
 }
 
 pub fn main(init: std.process.Init.Minimal) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    var stf = std.heap.stackFallback(heap_size, gpa.allocator());
+    const allocator = stf.get();
+    var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
     var runtime = std.Io.Threaded.init_single_threaded;
     defer runtime.deinit();
 
     const args = try init.args.toSlice(arena.allocator());
-    const cli = Cli.parse(args);
+    const cli = try Cli.parse(args);
 
     switch (cli.mode) {
         .Help => {
             try std.Io.File.stdout().writeStreamingAll(runtime.io(), usage);
         },
         .Encrypt => {
-            try encrypt(runtime.io(), gpa.allocator(), cli.input.?, cli.output);
+            try encrypt(runtime.io(), allocator, cli.input.?, cli.output);
         },
         .Decrypt => {
-            try decrypt(runtime.io(), gpa.allocator(), cli.input.?, cli.output);
+            try decrypt(runtime.io(), allocator, cli.input.?, cli.output);
         },
     }
 }
